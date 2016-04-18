21064d46320ddbaabc01e8948c1abb34b0f809f1
Rosen <Rosen9212@gmail.com>
2016-04-18 08:33:07 +0800

    model training task set

---
+++ model_train.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+echo "************************"
+echo "Model Training"
+echo "************************"
+# python model.py 7 lr 10 0.5
+# python model.py 7 lr 10 0.6
+# python model.py 7 lr 10 0.7
+# python model.py 7 lr 10 0.8
+#
+# python model.py 7 lr 20 0.5
+# python model.py 7 lr 20 0.6
+# python model.py 7 lr 20 0.7
+# python model.py 7 lr 20 0.8
+
+python model.py 7 rf 10 0.5
+python model.py 7 rf 10 0.6
+python model.py 7 rf 10 0.7
+python model.py 7 rf 10 0.8
+
+python model.py 7 rf 20 0.5
+python model.py 7 rf 20 0.6
+python model.py 7 rf 20 0.7
+python model.py 7 rf 20 0.8
