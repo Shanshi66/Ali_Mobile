3dcef13ab78a09feb0b5f2760945ed216b3c1340
Rosen <Rosen9212@gmail.com>
2016-04-20 21:32:38 +0800

    svm train

--- model_train.sh
+++ model_train.sh
@@ -12,12 +12,15 @@ echo "************************"
 # python model.py 7 lr 20 0.7
 # python model.py 7 lr 20 0.8
 
-python model.py 7 rf 10 0.5
-python model.py 7 rf 10 0.6
-python model.py 7 rf 10 0.7
-python model.py 7 rf 10 0.8
+# python model.py 7 rf 10 0.5
+# python model.py 7 rf 10 0.6
+# python model.py 7 rf 10 0.7
+# python model.py 7 rf 10 0.8
+#
+# python model.py 7 rf 20 0.5
+# python model.py 7 rf 20 0.6
+# python model.py 7 rf 20 0.7
+# python model.py 7 rf 20 0.8
 
-python model.py 7 rf 20 0.5
-python model.py 7 rf 20 0.6
-python model.py 7 rf 20 0.7
-python model.py 7 rf 20 0.8
+python model.py 7 svm 10 0.5
+python model.py 7 svm 20 0.5
